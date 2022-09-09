f406ef17c7f5ebeba5fdfa95e1894e33bd5b8970
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-10 00:02:12 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -61,3 +61,35 @@ var x = setInterval(function() {
 
 //Jam
 let scrollToTopRoundedfasfauserninjaXfa2xtextprimary=document.querySelector('div#row-no-guttersalign-items-center');let fasfauserninjaXfa2xtextprimary=document.querySelector('div#text-xsfont-weight-boldtext-uppercase-mb-1');setInterval(()=>{var widthdeVicewidthXinitialscalesHrinkno=new Date();const Jam= widthdeVicewidthXinitialscalesHrinkno.getHours().toString().padStart(2,0);const jam= widthdeVicewidthXinitialscalesHrinkno.getHours().toString().padStart(2,0);const menit= widthdeVicewidthXinitialscalesHrinkno.getMinutes().toString().padStart(2,0);const Menit = widthdeVicewidthXinitialscalesHrinkno.getMinutes().toString().padStart(2,0);const Detik = widthdeVicewidthXinitialscalesHrinkno.getSeconds().toString().padStart(2,0);const detik= widthdeVicewidthXinitialscalesHrinkno.getSeconds().toString().padStart(2,0);const jaM= widthdeVicewidthXinitialscalesHrinkno.getHours().toString().padStart(2,0);const mEnit= widthdeVicewidthXinitialscalesHrinkno.getMinutes().toString().padStart(2,0);const detIk= widthdeVicewidthXinitialscalesHrinkno.getSeconds().toString().padStart(2,0);if(Jam<4){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Dini Hari";}else if(jam<11){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Pagi";}else if(Jam<16){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Siang";}else if(jam<20){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Petang";}else{fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Malam";}scrollToTopRoundedfasfauserninjaXfa2xtextprimary.innerHTML=jaM+":"+mEnit+":"+detIk},250);
+
+// Tahun baruuuuuu
+
+var countDownDate = new Date("Jan 01, 2023 00:00:00").getTime();
+
+// Update Hitungan Mundur Setiap 1 Detik
+var x = setInterval(function() {
+
+    // Mengambil Tanggal Dan Waktu
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Kalkulator Waktu Hari, Jam, Menit, Detik 
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Menampilkan Hasil Di Dalam element Menggunakan id="time"
+    document.getElementById("tahunbaru").innerHTML = days + "Hari  " + hours + "Jam " + minutes + "Menit " + seconds + "Detik ";
+
+    // Ketika countdown timer sudah berakhir Maka Tulisan Berakhir
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("tahunbaru").innerHTML = "Berakhir";
+    }
+}, 1000);
+
+//Jam
+let scrollToTopRoundedfasfauserninjaXfa2xtextprimary=document.querySelector('div#row-no-guttersalign-items-center');let fasfauserninjaXfa2xtextprimary=document.querySelector('div#text-xsfont-weight-boldtext-uppercase-mb-1');setInterval(()=>{var widthdeVicewidthXinitialscalesHrinkno=new Date();const Jam= widthdeVicewidthXinitialscalesHrinkno.getHours().toString().padStart(2,0);const jam= widthdeVicewidthXinitialscalesHrinkno.getHours().toString().padStart(2,0);const menit= widthdeVicewidthXinitialscalesHrinkno.getMinutes().toString().padStart(2,0);const Menit = widthdeVicewidthXinitialscalesHrinkno.getMinutes().toString().padStart(2,0);const Detik = widthdeVicewidthXinitialscalesHrinkno.getSeconds().toString().padStart(2,0);const detik= widthdeVicewidthXinitialscalesHrinkno.getSeconds().toString().padStart(2,0);const jaM= widthdeVicewidthXinitialscalesHrinkno.getHours().toString().padStart(2,0);const mEnit= widthdeVicewidthXinitialscalesHrinkno.getMinutes().toString().padStart(2,0);const detIk= widthdeVicewidthXinitialscalesHrinkno.getSeconds().toString().padStart(2,0);if(Jam<4){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Dini Hari";}else if(jam<11){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Pagi";}else if(Jam<16){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Siang";}else if(jam<20){fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Petang";}else{fasfauserninjaXfa2xtextprimary.innerHTML="Selamat Malam";}scrollToTopRoundedfasfauserninjaXfa2xtextprimary.innerHTML=jaM+":"+mEnit+":"+detIk},250);
